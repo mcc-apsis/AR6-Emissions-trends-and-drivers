c0a20d7a9316fa3faa8d846c62d372e4c01e1100
Will Lamb <lamb.wf@gmail.com>
2019-05-17 09:40:28 +0200

    first run

---
+++ R/import.R
@@ -0,0 +1,11 @@
+
+rm(list = ls())
+library(xlsx)
+library(tidyverse)
+
+##### import data from matlab
+
+file.copy('C:/Users/lamw/Documents/SpiderOak Hive/Work/Code/MATLAB/Data shop/Aggregation/Basic data/basic.xls','Data/',overwrite=TRUE)
+
+basic<-read.xlsx('Data/basic.xls','data_full')
+save(basic,file='Data/basic.RData')
