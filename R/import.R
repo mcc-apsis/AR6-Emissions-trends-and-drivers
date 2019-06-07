d876db24a09102fa0009099399ed8a702b7a27e3
William Lamb <lamw@mcc-berlin.net>
2019-06-07 09:11:29 +0200

    update

--- R/import.R
+++ R/import.R
@@ -9,3 +9,7 @@ file.copy('C:/Users/lamw/Documents/SpiderOak Hive/Work/Code/MATLAB/Data shop/Agg
 
 basic<-read.xlsx('Data/basic.xls','data_full')
 save(basic,file='Data/basic.RData')
+
+
+#z<- read.delim('Data/labels_T.txt',sep = '\t',header = FALSE)
+#blarg <- data.frame(unique(z$V1))
