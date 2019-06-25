3b69918413f83d5cb381fdb460775a943764b721
William Lamb <lamw@mcc-berlin.net>
2019-06-25 15:28:21 +0200

    edgar plots

--- R/import.R
+++ R/import.R
@@ -10,6 +10,81 @@ file.copy('C:/Users/lamw/Documents/SpiderOak Hive/Work/Code/MATLAB/Data shop/Agg
 basic<-read.xlsx('Data/basic.xls','data_full')
 save(basic,file='Data/basic.RData')
 
+# 
+# z<- read.delim('Data/labels_T.txt',sep = '\t',header = FALSE)
+# blarg <- data.frame(unique(z$V1))
 
-#z<- read.delim('Data/labels_T.txt',sep = '\t',header = FALSE)
-#blarg <- data.frame(unique(z$V1))
+
+### EDGAR data
+
+rm(list = ls())
+library(tidyverse)
+
+edgar_CO2 <- openxlsx::read.xlsx('Data/IPCC emissions data AR6/19-06-14-v2fin-EDGAR GHG FT2017, main tables for IPCC.XLSX',
+                                 sheet='CO2',startRow=10)
+edgar_CO2 <- edgar_CO2[1:57]
+edgar_CO2 <- gather(edgar_CO2,Year,Value,'1970':'2017') %>% 
+  select(ISO_A3,Year,IPCC.detailed,CO2 = Value)
+
+edgar_CH4 <- openxlsx::read.xlsx('Data/IPCC emissions data AR6/19-06-14-v2fin-EDGAR GHG FT2017, main tables for IPCC.XLSX',
+                                 sheet='CH4',startRow=10)
+edgar_CH4 <- edgar_CH4[1:57]
+edgar_CH4 <- gather(edgar_CH4,Year,Value,'1970':'2017') %>% 
+  select(ISO_A3,Year,IPCC.detailed,CH4 = Value)
+
+
+edgar_N2O <- openxlsx::read.xlsx('Data/IPCC emissions data AR6/19-06-14-v2fin-EDGAR GHG FT2017, main tables for IPCC.XLSX',
+                                 sheet='N2O',startRow=10)
+edgar_N2O <- edgar_N2O[1:58]
+edgar_N2O <- gather(edgar_N2O,Year,Value,'1970':'2017') %>% 
+  select(ISO_A3,Year,IPCC.detailed,N2O = Value)
+
+edgar_Fgas <- openxlsx::read.xlsx('Data/IPCC emissions data AR6/19-06-14-v2fin-EDGAR GHG FT2017, main tables for IPCC.XLSX',
+                                 sheet='Fgas',startRow=10)
+edgar_Fgas <- edgar_Fgas[1:58]
+edgar_Fgas <- edgar_Fgas[1:1707,]
+edgar_Fgas <- gather(edgar_Fgas,Year,Value,'1970':'2017')%>% 
+  select(ISO_A3,Year,IPCC.detailed,Fgas = Value) %>% 
+  mutate(Fgas = as.numeric(Fgas))
+
+
+edgar_GHG <- left_join(edgar_CO2,edgar_CH4)
+edgar_GHG <- left_join(edgar_GHG,edgar_N2O)
+edgar_GHG <- left_join(edgar_GHG,edgar_Fgas)
+
+
+#### calculate total GHG
+edgar_GHG <- edgar_GHG %>% 
+  group_by(ISO_A3,Year,IPCC.detailed) %>% 
+  mutate(GHG = sum(CO2,CH4,N2O,Fgas,na.rm=T)) %>% 
+  ungroup() %>% 
+  mutate(Year=as.numeric(Year))
+
+
+#### join source categories
+categories <- read.xlsx('Data/IPCC Mapping.xlsx',sheetName='EmissionMap',startRow=3)
+sectors <- read.xlsx('Data/IPCC Mapping.xlsx',sheetName='Sectors')
+
+categories <- left_join(categories,sectors,by=c("Sector"="code"))
+categories <- left_join(categories,sectors,by=c("Chapter"="code")) %>% 
+  select(IPCC.detailed=IPCC.cat.,IPCC.description=IPCC_description,Sector=sector.x,Chapter=sector.y)
+
+edgar_GHG <- left_join(edgar_GHG,categories,by=c("IPCC.detailed"="IPCC.detailed"))
+
+edgar_GHG <- edgar_GHG %>% 
+  select(ISO=ISO_A3,IPCC.detailed,IPCC.description,Sector,Chapter,Year,everything())
+
+
+#### calculate total sector emissions
+totals <- gather(edgar_GHG,key,value,CO2:GHG)
+totals <- totals %>% 
+  group_by(ISO,Year,key) %>% 
+  summarise(value=sum(value,na.rm=T)) %>% 
+  mutate(Chapter="Total",Sector="Total",IPCC.detailed="Total",IPCC.description="Total")
+totals <- spread(totals,key,value) %>% 
+  select(ISO,IPCC.detailed,IPCC.description,Sector,Chapter,Year,CO2,CH4,N2O,Fgas,GHG) %>% 
+  ungroup()
+
+edgar_GHG <- rbind(edgar_GHG,totals)
+
+save(edgar_GHG,file='Data/edgar.RData')
--- R/old_code.R
+++ R/old_code.R
@@ -213,4 +213,297 @@ blarg %>%
 ```
 
 
+```{r regional_trends, echo=FALSE,warning=FALSE,fig.width=8,fig.height=10,fig.path="Plots/",dev=c('png','pdf')}
 
+selection = c("co2_terr_GCB","pop_UN")
+time_start = 2010
+
+##############  calculate growth rates in given time period ############## 
+
+rates <- basic %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+####### CHECK DATA HERE FOR ZEROS AND NANS ####### 
+
+## remove countries below absolute emissions threshold
+
+rates <- rates %>% 
+  group_by(Country) %>% 
+  mutate(rate=(last(co2_terr_GCB)/first(co2_terr_GCB))^(1/(last(Year)-time_start+1))-1) %>% 
+  filter(Year==2017) %>% 
+  na.omit() %>% 
+  arrange(desc(co2_terr_GCB))
+
+# what is 80% of the emissions?
+threshold <- basic %>% 
+  filter(Country=="World") %>% 
+  filter(Year==2017) %>% 
+  summarise_at(selection,funs(.*0.80))
+
+threshold <- as.vector(threshold[,grep(selection[1],colnames(threshold))])
+rates$cutoff <- 0
+z <- 0
+for (i in 1:length(rates$Country)){
+  z <- z + rates$co2_terr_GCB[i]
+  if (z > threshold) {
+    break
+  }
+  rates$cutoff[i] <- 1
+}
+
+country_list <- rates %>% select(Country,cutoff)
+
+#### calculate growth rates for countries below threshold, grouped by region ####
+
+blarg <- basic %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+blarg <- left_join(blarg,country_list,by=c("Country"="Country")) %>% 
+  filter(cutoff==0) %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(WB.income,Year) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+blarg <- blarg %>% 
+  group_by(WB.income) %>% 
+  mutate(rate=(last(co2_terr_GCB)/first(co2_terr_GCB))^(1/(last(Year)-time_start+1))-1) %>% 
+  filter(Year==2017) %>% 
+  na.omit() %>% 
+  mutate(Country=paste(WB.income,"(rest)"))
+
+
+### join and plot
+rates <- rbind(rates %>% filter(cutoff==1) %>% select(-cutoff),blarg)
+
+p2 <- rates %>% 
+  ggplot(.,aes(x = reorder(Country,rate), y = rate*100, fill=WB.income)) +
+  geom_bar(stat='identity') + 
+  ylab(paste("Rate of change in emissions, ",time_start,"- 2017")) +
+  coord_flip() +
+  #facet_grid(WB.income~.,scales="free_y") +
+  theme(legend.position="none",
+        axis.title.y = element_blank()) 
+
+
+############## plot absolute values, trend over time ############## 
+
+regions <- left_join(basic,country_list,by=c("Country"="Country")) %>%
+  filter(!is.na(WB.income)) %>% 
+  filter(Country!="World") %>% 
+  filter(Year>1980 & Year<2018) %>% 
+  filter(cutoff==0) %>% 
+  group_by(WB.income,Year) %>% 
+  summarise_at(selection,sum,na.rm=TRUE) %>% 
+  mutate(Country=paste(WB.income,"(rest)")) %>% 
+  ungroup()
+
+countries <- left_join(basic,country_list,by=c("Country"="Country")) %>%
+  filter(!is.na(WB.income)) %>% 
+  filter(Country!="World") %>% 
+  filter(Year>1980 & Year<2018) %>% 
+  filter(cutoff==1) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE) %>% 
+  ungroup()
+
+plot_data <- rbind(countries,regions)
+
+rect <- data.frame(xmin=time_start, xmax=2017, ymin=-Inf, ymax=Inf)
+
+# plot_data %>% 
+#   #filter(WB.income!="High income") %>% 
+#   ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+#   geom_area(color="black")
+
+#plot_data %>% 
+# ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+#geom_bar(stat='identity')
+
+p1 <- regions %>%
+  ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+  geom_area() +
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="grey20",
+            alpha=0.35,
+            inherit.aes = FALSE) +
+  theme(legend.position='bottom',
+        legend.title=element_blank())
+
+
+ggarrange(p1,p2,ncol=1,nrow=2)
+
+```
+
+
+
+# fossil_data <- fft %>% 
+#   select(-Per.Capita,-Total) %>% 
+#   group_by(Year) %>% 
+#   mutate_at(vars(-group_cols()),funs(.*3.664)) %>% 
+#   mutate_at(vars(-group_cols()),funs(./1000))
+
+# budget_data <- gcb %>% 
+#   select(-ocean.sink,-land.sink,-atmospheric.growth,-budget.imbalance) %>% 
+#   group_by(Year) %>% 
+#   mutate_at(vars(-group_cols()),funs(.*3.664))
+
+# plot_data <- left_join(budget_data,fossil_data,by=c("Year"="Year")) %>% 
+#   select(-fossil.fuel.and.industry)
+# 
+# plot_data <- gather(plot_data,key,value,-Year)
+# plot_data$key <- gsub("land.use.change.emissions","Land use change",plot_data$key)
+# plot_data$key <- as.factor(plot_data$key)
+# plot_data$key <- factor(plot_data$key,levels(plot_data$key)[c(3,1,4,2,6,5)])
+# 
+# 
+# p1 <- plot_data %>% 
+#   ggplot(.,aes(x=Year,y=value,fill=key)) +
+#   geom_area() +
+#   ylab("Fossil fuel, cement & land use change emissions (GtCO2/yr)") +
+#   theme(legend.position = c(0.15,0.75),
+#         legend.title = element_blank(),
+#         axis.title.x = element_blank())
+
+
+
+
+
+
+
+
+
+``` {blarg}
+
+selection = c("co2_terr_GCB","pop_UN")
+time_start = 2010
+
+##############  calculate growth rates in given time period ############## 
+
+rates <- basic %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+####### CHECK DATA HERE FOR ZEROS AND NANS ####### 
+
+## remove countries below absolute emissions threshold
+
+rates <- rates %>% 
+  group_by(Country) %>% 
+  mutate(rate=(last(co2_terr_GCB)/first(co2_terr_GCB))^(1/(last(Year)-time_start+1))-1) %>% 
+  filter(Year==2017) %>% 
+  na.omit() %>% 
+  arrange(desc(co2_terr_GCB))
+
+# what is 80% of the emissions?
+threshold <- basic %>% 
+  filter(Country=="World") %>% 
+  filter(Year==2017) %>% 
+  summarise_at(selection,funs(.*0.80))
+
+threshold <- as.vector(threshold[,grep(selection[1],colnames(threshold))])
+rates$cutoff <- 0
+z <- 0
+for (i in 1:length(rates$Country)){
+  z <- z + rates$co2_terr_GCB[i]
+  if (z > threshold) {
+    break
+  }
+  rates$cutoff[i] <- 1
+}
+
+country_list <- rates %>% select(Country,cutoff)
+
+#### calculate growth rates for countries below threshold, grouped by region ####
+
+blarg <- basic %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+blarg <- left_join(blarg,country_list,by=c("Country"="Country")) %>% 
+  filter(cutoff==0) %>% 
+  filter(!is.na(WB.income)) %>% 
+  filter(Year>=time_start & Year<2018) %>% 
+  group_by(WB.income,Year) %>% 
+  summarise_at(selection,sum,na.rm=TRUE)
+
+blarg <- blarg %>% 
+  group_by(WB.income) %>% 
+  mutate(rate=(last(co2_terr_GCB)/first(co2_terr_GCB))^(1/(last(Year)-time_start+1))-1) %>% 
+  filter(Year==2017) %>% 
+  na.omit() %>% 
+  mutate(Country=paste(WB.income,"(rest)"))
+
+
+### join and plot
+rates <- rbind(rates %>% filter(cutoff==1) %>% select(-cutoff),blarg)
+
+p2 <- rates %>% 
+  ggplot(.,aes(x = reorder(Country,rate), y = rate*100, fill=WB.income)) +
+  geom_bar(stat='identity') + 
+  ylab(paste("Rate of change in emissions, ",time_start,"- 2017")) +
+  coord_flip() +
+  #facet_grid(WB.income~.,scales="free_y") +
+  theme(legend.position="none",
+        axis.title.y = element_blank()) 
+
+
+############## plot absolute values, trend over time ############## 
+
+regions <- left_join(basic,country_list,by=c("Country"="Country")) %>%
+  filter(!is.na(WB.income)) %>% 
+  filter(Country!="World") %>% 
+  filter(Year>1980 & Year<2018) %>% 
+  filter(cutoff==0) %>% 
+  group_by(WB.income,Year) %>% 
+  summarise_at(selection,sum,na.rm=TRUE) %>% 
+  mutate(Country=paste(WB.income,"(rest)")) %>% 
+  ungroup()
+
+countries <- left_join(basic,country_list,by=c("Country"="Country")) %>%
+  filter(!is.na(WB.income)) %>% 
+  filter(Country!="World") %>% 
+  filter(Year>1980 & Year<2018) %>% 
+  filter(cutoff==1) %>% 
+  group_by(Country,Year,WB.income) %>% 
+  summarise_at(selection,sum,na.rm=TRUE) %>% 
+  ungroup()
+
+plot_data <- rbind(countries,regions)
+
+rect <- data.frame(xmin=time_start, xmax=2017, ymin=-Inf, ymax=Inf)
+
+# plot_data %>% 
+#   #filter(WB.income!="High income") %>% 
+#   ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+#   geom_area(color="black")
+
+#plot_data %>% 
+# ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+#geom_bar(stat='identity')
+
+p1 <- regions %>%
+  ggplot(.,aes(x=Year,y=co2_terr_GCB,fill=WB.income)) +
+  geom_area() +
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="grey20",
+            alpha=0.35,
+            inherit.aes = FALSE) +
+  theme(legend.position='bottom',
+        legend.title=element_blank())
+
+
+ggarrange(p1,p2,ncol=1,nrow=2)
+
+
+```
