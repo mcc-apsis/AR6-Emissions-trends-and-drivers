824599e43dabd09255dbeaa21e057d79f212f62c
William F. Lamb <lamb.wf@gmail.com>
2021-03-31 09:56:11 +0200

    decarbonising countries update

--- R/Data preparation/import_land_data.R
+++ R/Data preparation/import_land_data.R
@@ -6,16 +6,26 @@ library(openxlsx)
 #houghton <- read.xlsx("Data/Land and GCB/Country_ELUC_26082020.xlsx",sheet = "H&N_2017_IPCC_regions",startRow=1,colNames=TRUE)
 #houghton <- read.xlsx("Data/Land and GCB/Country_ELUC_26082020_HNExtrapolated.xlsx",sheet = "data",startRow=1,colNames=TRUE)
 
-blue <- read.xlsx("Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="BLUE_GCB2020_IPCC_regions")
-houghton <- read.xlsx("Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="H&N_2017_IPCC_regions")
-oscar <- read.xlsx("Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="OSCAR_IPCC_regions")
+#blue_old <- read.xlsx("../../Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="BLUE_GCB2020_IPCC_regions")
+#houghton <- read.xlsx("../../Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="H&N_2017_IPCC_regions")
+#oscar <- read.xlsx("../../Data/Land and GCB/Country_ELUC_25112020_forWill.xlsx",sheet="OSCAR_IPCC_regions")
+
+blue <- read.xlsx("../../Data/Land and GCB/Country_ELUC_23032021.xlsx",sheet="BLUE_GCB2020_IPCC_regions")
+houghton <- read.xlsx("../../Data/Land and GCB/Country_ELUC_23032021.xlsx",sheet="H&N_2017_IPCC_regions")
+oscar <- read.xlsx("../../Data/Land and GCB/Country_ELUC_23032021.xlsx",sheet="oscar_10ipccregions")
+
+#load("../../Data/land.RData")
+#land_old<-land
 
 blue <- gather(blue,region_ar6_10,blue,`Africa`:`Southern.Asia`) %>% 
-  select(region_ar6_10,year=X1,blue)
+  select(region_ar6_10,year=X1,blue) %>%
+  filter(!is.na(year))
 houghton <- gather(houghton,region_ar6_10,houghton,`Africa`:`Southern.Asia`) %>% 
-  select(region_ar6_10,year=X1,houghton)
+  select(region_ar6_10,year=X1,houghton) %>%
+  filter(!is.na(year))
 oscar <- gather(oscar,region_ar6_10,oscar,`Africa`:`Southern.Asia`) %>% 
-  select(region_ar6_10,year=X1,oscar)
+  select(region_ar6_10,year=X1,oscar) %>%
+  filter(!is.na(year))
 
 land <- left_join(blue,houghton,by = c("region_ar6_10", "year"))
 land <- left_join(land,oscar,by = c("region_ar6_10", "year"))
@@ -34,11 +44,28 @@ land <- land %>%
 # check country names and regions
 land$region_ar6_10 <- gsub("[.]"," ",land$region_ar6_10)
 
-load('Data/ipcc_regions.RData')
+load('../../Data/ipcc_regions.RData')
 
 regions <- ipcc_regions %>% select(region_ar6_10) %>% distinct()
 
 uhoh <- anti_join(land,regions,by="region_ar6_10")
+
+#check updated data vs. previous data
+# colnames(land_old)[3:6]<-paste(colnames(land_old)[3:6],"_old",sep = "")
+# comp<-left_join(land,land_old,by=c("year","region_ar6_10"))
+# p1<-comp %>% 
+#   filter(region_ar6_10==regions[2,1])%>%
+#   ggplot() + 
+#   geom_line(mapping=aes(x=year,y=blue))+
+#   geom_line(mapping=aes(x=year,y=blue_old,color="red"))
+# p1
+# 
+# check <-comp %>% 
+#   filter(region_ar6_10=="South-East Asia and Developing Pacific")%>%
+#   filter(year>1975) %>%
+#   mutate(blue_diff=blue-blue_old) %>%
+#   mutate(blue_diff_rate=(blue-blue_old)/blue_old) %>%
+#   select(region_ar6_10,year,blue,blue_old,blue_diff,blue_diff_rate)
 
 wb <- createWorkbook()
 info = data.frame(x=c("Author","Units","Date","Contact"),y=c("Julia Pongratz","tCO2",as.character(Sys.Date()),"Lamb@mcc-berlin.net"))
